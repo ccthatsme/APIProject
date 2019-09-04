f67bde9fac533e09101fa1d46fd9b88c9fb080ff
Chris <cciric44@gmail.com>
2019-09-04 11:13:58 -0400

    controller test

--- src/main/java/co/grandcircus/APIProject/controller/RecipeController.java
+++ src/main/java/co/grandcircus/APIProject/controller/RecipeController.java
@@ -1,8 +1,22 @@
 package co.grandcircus.APIProject.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class RecipeController {
+	
+	RestTemplate rt = new RestTemplate();
+	
+	@RequestMapping("/")
+	public ModelAndView home() {
+		String url = "http://www.recipepuppy.com/api/";
+		
+		String recipe = rt.getForObject(url, String.class);
+		ModelAndView mv = new ModelAndView("index", "test", recipe);
+		return mv;
+	}
 	
 }
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -10,5 +10,7 @@
 </head>
 <body>
 	<h1>Hello, world</h1>
+	
+	${test }
 </body>
 </html>
